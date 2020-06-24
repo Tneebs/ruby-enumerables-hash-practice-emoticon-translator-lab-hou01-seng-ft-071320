ad22aa2581df072498cb0f75baf0ac78a80d53d0
Ted Neben <tedneben@gmail.com>
2020-06-24 19:58:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,6 +22,7 @@ end
 def get_english_meaning(file_path, j_emoticons)
   library = load_library(file_path)        # code goes here
   result = library.each do |emotion,lang|
+    lang.each do |
   binding.pry
 end
 end

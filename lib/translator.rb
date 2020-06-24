56f8b4d7c5384b87ce37e7d6e2c04cdcd7f13086
Ted Neben <tedneben@gmail.com>
2020-06-24 20:16:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,31 +19,31 @@ def get_japanese_emoticon
   # code goes here
 end
 
-# def get_english_meaning(file_path, j_emoticons)
-#   library = load_library(file_path)        # code goes here
-#   result = library.each do |emotion,lang|
-#     lang.each do |inner_key,emote|
-#       binding.pry
-#   end
-# end
-# end
-
-def get_english_meaning(file,j_emoticon)
-  emoticons = load_library(file)
-  eng_meaning = ""
-  apology = "Sorry, that emoticon was not found"
-  emoticons.each do |emotion, lang|
-    # binding.pry
-    lang.each do |inner_key,emoticon|
-      if emoticon == j_emoticon
-        eng_meaning = emotion
-        # binding.pry
-      end
-    end
-    if eng_meaning == ""
+def get_english_meaning(file_path, j_emoticons)
+  library = load_library(file_path)        # code goes here
+  result = library.each do |emotion,lang|
+    lang.each do |inner_key,emote|
       binding.pry
-      eng_meaning = apology
-    end  
   end
-  eng_meaning
 end
+end
+
+# def get_english_meaning(file,j_emoticon)
+#   emoticons = load_library(file)
+#   eng_meaning = ""
+#   apology = "Sorry, that emoticon was not found"
+#   emoticons.each do |emotion, lang|
+#     # binding.pry
+#     lang.each do |inner_key,emoticon|
+#       if emoticon == j_emoticon
+#         eng_meaning = emotion
+#         # binding.pry
+#       end
+#     end
+#     if eng_meaning == ""
+#       binding.pry
+#       eng_meaning = apology
+#     end  
+#   end
+#   eng_meaning
+# end

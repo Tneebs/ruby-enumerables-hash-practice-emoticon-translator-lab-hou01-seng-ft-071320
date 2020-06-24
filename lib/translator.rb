b7ae32bde54e1252b0a6a0246d472690934b5b5a
Ted Neben <tedneben@gmail.com>
2020-06-24 20:46:10 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -33,6 +33,7 @@ def get_japanese_emoticon(file_path, e_emoticons)
 end
 
 def get_english_meaning(file_path, j_emoticons)
+  binding.pry
   library = load_library(file_path)        # code goes here
   english = ""
   apology = "Sorry, that emoticon was not found"
